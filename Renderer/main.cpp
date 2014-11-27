83001f956a583be93ac5f33f0416e225bd3c5f34
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-27 16:13:02 +0100

    added make_vbo, make_vao, added example cube class

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include "GLUtils.h"
+#include "Cube.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    int w, h;
+    GLFWwindow* win;
+    
+    GLUtils::create_window("OpenGL", 480, 320, win);
+    
+    auto sh = GLUtils::compile_shaders_from_file("s.vertex", "s.fragment");
+    
+    GLuint program;
+    if (GLUtils::link_shaders(sh, program)) {
+        Cube cube(10, 10, 10);
+        cube.setup(program);
+        
+        while (!glfwWindowShouldClose(win)) {
+            glfwGetWindowSize(win, &w, &h);
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+            glViewport(0, 0, w, h);
+            glClearColor(0.0f, 0.0f, 0.0f, 0.8f);
+            glUseProgram(program);
+            
+            cube.draw();
+            
+            glfwPollEvents();
+            glfwSwapBuffers(win);
+        }
+    }
 }
