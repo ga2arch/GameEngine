37a7a306e43b45213f0872829af1a437a3af6dec
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-27 14:55:35 +0100

    first

---
+++ Renderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Renderer
+//
+//  Created by Gabriele Carrettoni on 27/11/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
