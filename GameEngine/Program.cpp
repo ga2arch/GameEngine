780fba5cd0e16c156b60c8a0203552d4e6c39ec9
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-12-05 20:57:14 +0100

    shadows

--- GameEngine/Program.cpp
+++ GameEngine/Program.cpp
@@ -2,50 +2,8 @@
 //  Program.cpp
 //  GameEngine
 //
-//  Created by Gabriele Carrettoni on 29/11/14.
+//  Created by Gabriele Carrettoni on 04/12/14.
 //  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
 //
 
 #include "Program.h"
-
-Program::Program(Shader vertex, Shader fragment) {
-    
-    auto shaders = std::make_pair(vertex(), fragment());
-    GLUtils::link_shaders(shaders, program);
-    
-    add_uniform("proj");
-    add_uniform("view");
-    add_uniform("model");
-    
-    add_attribute("position");
-    add_attribute("normal");
-    add_attribute("uv");
-    
-    add_uniform("light_pos");
-    add_uniform("irr");
-    add_uniform("Ks");
-    
-    add_uniform("tex");
-}
-
-void Program::use() {
-    glUseProgram(program);
-}
-
-void Program::add_attribute(const std::string name) {
-    auto a = glGetAttribLocation(program, name.c_str());
-    attributes.insert(std::make_pair(name, a));
-}
-
-void Program::add_uniform(const std::string name) {
-    auto a = glGetUniformLocation(program, name.c_str());
-    uniforms.insert(std::make_pair(name, a));
-}
-
-GLuint Program::get_attribute(const std::string name) {
-    return attributes.at(name);
-}
-
-GLuint Program::get_uniform(const std::string name) {
-    return uniforms.at(name);
-}
--- GameEngine/Program.h
+++ GameEngine/Program.h
@@ -2,41 +2,67 @@
 //  Program.h
 //  GameEngine
 //
-//  Created by Gabriele Carrettoni on 29/11/14.
+//  Created by Gabriele Carrettoni on 04/12/14.
 //  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
 //
 
 #ifndef __GameEngine__Program__
 #define __GameEngine__Program__
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <stdio.h>
-#include <map>
+#include <iostream>
 #include "GLUtils.h"
-#include "Shader.h"
+
+class Shader {
+    
+public:
+    Shader() {};
+    
+    Shader(const std::string source, GLenum type): type(type) {
+        s = GLUtils::compile_shader_from_file(source, type);
+    }
+  
+    GLuint operator()() { return s; }
+    
+private:
+    GLenum type;
+    GLuint s;
+    
+};
 
 class Program {
     
 public:
-    Program(Shader vertex, Shader fragment);
+    Program() {};
     
-    void use();
-    void add_uniform(const std::string name);
-    void add_attribute(const std::string name);
-    
-    GLuint get_uniform(const std::string name);
-    GLuint get_attribute(const std::string name);
-    
-    bool operator<(const Program& p) const {
-        return program < p.program;
+    Program(Shader v, Shader f): v(v), f(f) {
+        auto shs = std::make_pair(v(), f());
+        GLUtils::link_shaders(shs, p);
     };
-
-    GLuint program;
     
-protected:
+    void use() { glUseProgram(p); }
     
-    std::map<std::string, GLuint> uniforms;
-    std::map<std::string, GLuint> attributes;
+    void set_uniform(const char* name, glm::mat4 v) {
+        auto u = glGetUniformLocation(p, name);
+        glUniformMatrix4fv(u, 1, GL_FALSE, glm::value_ptr(v));
+    }
+    
+    void set_uniform(const char* name, int v) {
+        auto u = glGetUniformLocation(p, name);
+        glUniform1i(u, v);
+    }
 
+    
+private:
+    Shader v;
+    Shader f;
+    
+    GLuint p;
+    
 };
 
 #endif /* defined(__GameEngine__Program__) */
